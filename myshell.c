13c22f009019b73b9838e9ddc31953b734144c85
stevetolvaj <stevetolvaj@gmail.com>
2021-10-07 15:07:30 -0400

    Implemented single redirection

--- myshell.c
+++ myshell.c
@@ -13,13 +13,14 @@ void parser(char *input, char **args);
 int checkParsedArgs(char **args);
 int redirect(char **args, int position);
 int redirectAppend(char **args, int position);
-void redirectInput(char *arg1, char *arg2);
+int redirectInput(char **args, int position);
+int execute(char **args, int position);
 
 const char error_message[30]="An error has occurred\n";
 
 int main(int argc, char const *argv[], char const *envp[])
 {
-
+    FILE *input;
     char *args[MAX_ARGS];    // The arguments extracted from stdin or batch file after parsing.
     char *buff;     // The buffer used for getline().
     size_t size = 0;// Size used for getline().
@@ -32,22 +33,22 @@ int main(int argc, char const *argv[], char const *envp[])
     }
     // If filename is specified in arguments set the file descriptor to stdin descriptor.
     if (argc == 2) { 
-        int fd = open(argv[1], O_RDONLY);
-        dup2(fd, STDIN_FILENO);
-        close(fd);
+        input = fopen(argv[1], "r");
+       
         fileSpecifiedFlag = 1;
 
         // Exit with code 1 and specified error message if bad batch file.
-        if (fd == -1) {
+        if (input == NULL) {
             write(STDERR_FILENO,error_message,strlen(error_message));
             exit(1);
         }
     } else {    // Only print prompt if no batch file entered.
+        input = stdin;
         printf("%s/myshell>", getenv("PWD"));
     }
 
     // Read input from stdin.
-    while(getline(&buff, &size, stdin) != -1) {
+    while(getline(&buff, &size, input) != -1) {
         // Replace newline with \0 if exists.
         if(strchr(buff, '\n') != NULL) {
             buff[strlen(buff) - 1] = '\0';
@@ -59,7 +60,6 @@ int main(int argc, char const *argv[], char const *envp[])
         }
         parser(buff, args);
         checkParsedArgs(args);
-        printf("%s\n", buff);
         // Only print prompt if no batch file entered.
         if (!fileSpecifiedFlag) {
              printf("%s/myshell>", getenv("PWD"));
@@ -68,6 +68,7 @@ int main(int argc, char const *argv[], char const *envp[])
     }
     
     free(buff);
+    
     
     return 0;
 }
@@ -90,7 +91,9 @@ void parser(char *input, char **args) {
 
 int checkParsedArgs(char **args) {
     int i = 0;
-    
+    // File descriptor to save prev output.
+    int foundSpecial = 0;
+
     while(args[i] != NULL) {
         if(strcmp(args[i], ">") == 0) {
             // Set terminating position of args so exec knows when to stop.
@@ -98,6 +101,7 @@ int checkParsedArgs(char **args) {
             if (redirect(args, i) == -1) {
                 write(STDERR_FILENO,error_message,strlen(error_message));
             }
+            foundSpecial = 1;
         }
         else if(strcmp(args[i], ">>") == 0) {
             // Set terminating position of args so exec knows when to stop.
@@ -105,10 +109,25 @@ int checkParsedArgs(char **args) {
             if (redirectAppend(args, i) == -1) {
                 write(STDERR_FILENO,error_message,strlen(error_message));
             }
-        }
-
+            foundSpecial = 1;
+        } else if(strcmp(args[i], "<") == 0) {
+            args[i] = NULL;
+            if (redirectInput(args, i) == -1) {
+                write(STDERR_FILENO,error_message,strlen(error_message));
+            }
+            foundSpecial = 1;
+        } 
         i++;
     }
+
+    if (!foundSpecial)
+    {
+        if (execute(args, i) == -1) {
+            write(STDERR_FILENO,error_message,strlen(error_message));
+        }
+    }
+    
+
     return 0;
 }
 
@@ -120,7 +139,7 @@ int checkParsedArgs(char **args) {
  * @param args The line of arguments from user input.
  * @param position The position of the first null located in args.
  * 
- * @return Will return -1 if failure occurred or 0 if successfull.
+ * @return Will return -1 if failure occurred or 0 if successful.
 **/
 int redirect(char **args, int position) {
     
@@ -135,16 +154,20 @@ int redirect(char **args, int position) {
     if (rc < 0)
     {
         return -1;
-    }
-
-    if (rc == 0) {
+    } else if (rc == 0) {
         if (dup2(fd, STDOUT_FILENO) == -1) {
-            return -1;
+            write(STDERR_FILENO,error_message,strlen(error_message));
+            exit(1);
         }
         close(fd);
-        execvp(args[0], args);
+        if (execvp(args[0], args) == -1) {
+            write(STDERR_FILENO,error_message,strlen(error_message));
+            exit(1);
+        }
+    } else {
+        wait(NULL);
     }
-    wait(NULL);
+   
     return 0;
 }
 
@@ -157,10 +180,9 @@ int redirect(char **args, int position) {
  * @param args The line of arguments from user input.
  * @param position The position of the first null located in args.
  * 
- * @return Will return -1 if failure occurred or 0 if successfull.
+ * @return Will return -1 if failure occurred or 0 if successful.
 **/
 int redirectAppend(char **args, int position) {
-    printf("%s\n", args[position + 1]);
     int fd = open(args[position + 1], O_CREAT | O_WRONLY | O_APPEND);
 
     if(fd == -1) {
@@ -172,19 +194,94 @@ int redirectAppend(char **args, int position) {
     if (rc < 0)
     {
         return -1;
-    }
-
-    if (rc == 0) {
+    } else if (rc == 0) {
         if (dup2(fd, STDOUT_FILENO) == -1) {
-            return -1;
+            write(STDERR_FILENO,error_message,strlen(error_message));
+            exit(1);
         }
         close(fd);
-        execvp(args[0], args);
+        if (execvp(args[0], args) == -1) {
+            write(STDERR_FILENO,error_message,strlen(error_message));
+            exit(1);
+        }
+    } else {
+        wait(NULL);
     }
-    wait(NULL);
+    
     return 0;
 }
 
-void redirectInput(char *arg1, char *arg2) {
+int redirectInput(char **args, int position) {
+    // int prev;
+    // int redirectOut = 0;
+    // int outFd;
+
+    // if(strcmp(args[position + 2], ">") == 0) {
+    //     redirectOut = 1;
+    //     prev = dup(STDOUT_FILENO);
+    //     outFd = open(args[position + 3], O_WRONLY| O_CREAT | O_TRUNC);
+    // } 
+    // if(strcmp(args[position + 2], ">>") == 0) {
+    //     redirectOut = 2;
+    //     outFd = open(args[position + 3], O_WRONLY| O_CREAT | O_APPEND);
+    //     prev = dup(STDOUT_FILENO);
+    // } 
+    
+
+    int fd = open(args[position + 1], O_RDONLY);
+    
+
+    if(fd == -1) {
+        return -1;
+    }
+ 
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        return -1;
+    } else if (rc == 0) {
+        
+        if (dup2(fd, STDIN_FILENO) == -1) {
+            write(STDERR_FILENO,error_message,strlen(error_message));
+            exit(1);
+        }
+        close(fd);
+        if (execvp(args[0], args) == -1) {
+            write(STDERR_FILENO,error_message,strlen(error_message));
+            exit(1);
+        }
+
+        // if((redirectOut == 1) | (redirectOut == 2)) {
+        //     dup2(outFd, prev);
+            
+        //     close(outFd);
+        // }
+
+    } else {
+      //  close(prev);
+        wait(NULL);
+    }
+    
+    return 0;
+}
+
+int execute(char **args, int position) {
+    
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        return -1;
+    } else if (rc == 0) { 
+        if (execvp(args[0], args) == -1) {
+            write(STDERR_FILENO,error_message,strlen(error_message));
+            exit(1);
+        }
+    } else {
+        wait(NULL);
+    }
+    
+    return 0;
     
 }
